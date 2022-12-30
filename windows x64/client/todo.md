b50fae2dcc1022ecd739d0cfb1ea6c39937595f1
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2022-12-30 13:47:27 -0600

    server stuff

---
+++ windows x64/client/todo.md
@@ -0,0 +1,6 @@
+# CLIENT TODO
+
+1. Finish server logging,
+2. Gameloop
+3. Controlls
+4. Player state management
---
+++ windows x64/server/example_mod/myserver.js
@@ -0,0 +1,4 @@
+var pocketbase = require('pocketbase')
+
+// export so client can grab the api url
+export var myserver = new pocketbase('api backend')
